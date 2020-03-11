b76542debdcbd314ceae43b8a51e89b0d5b06844
wadikk83 <wadikk83@gmail.com>
2020-03-12 00:29:47 +0300

    Task25

---
+++ src/com/company/Task25.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class Task25 {
+    /**
+     * Создать иерархию классов, описывающих банковские карточки.
+     * Иерархия должны иметь хотя бы 3 уровня
+     */
+    public static void main(String[] args) {
+        // write your code here
+
+
+    }
+}
