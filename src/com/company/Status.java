391e141e543a91bab96de44a820b19050853e5e9
wadikk83 <wadikk83@gmail.com>
2020-03-12 00:31:14 +0300

    Task25 ready

---
+++ src/com/company/Status.java
@@ -0,0 +1,22 @@
+package com.company;
+
+public enum Status {
+    GOLD(1,"Super"),
+    PLATINA(2,"Wow");
+
+    int id;
+    String status;
+
+    Status(int id, String status) {
+        this.id = id;
+        this.status = status;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+}
