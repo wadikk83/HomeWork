533485446cae3039bd0584e52591da26160f8c73
wadikk83 <wadikk83@gmail.com>
2020-04-09 02:30:14 +0300

    Задача к 15 лекции про книги

---
+++ src/main/java/by/home/chapter1/hometaskbylesson15/FileOperation.java
@@ -0,0 +1,14 @@
+package by.home.chapter1.hometaskbylesson15;
+
+import java.util.List;
+
+public interface FileOperation {
+    public void createFile(String url, List<Book> books);
+
+    public List<Book> readFile(String url);
+
+    public boolean updateFile(String url, List<Book> books);
+
+    public void deleteFile(String url);
+
+}
---
+++ src/main/java/by/home/chapter1/hometaskbylesson15/Main.java
@@ -0,0 +1,33 @@
+package by.home.chapter1.hometaskbylesson15;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Main {
+    public static void main(String[] args) {
+        final String url = "./books/BookList.obj";
+
+        List<Book> books = new ArrayList<>();
+        books.add(new Book("Author1", "Title1"));
+        books.add(new Book("Author2", "Title2"));
+        books.add(new Book("Author3", "Title3"));
+        books.add(new Book("Author2", "Title4"));
+        books.add(new Book("Author1", "Title5"));
+        books.add(new Book("Author1", "Title6"));
+
+        FileService fileService = new FileService();
+        fileService.createFile(url, books);
+
+        List<Book> books1 = fileService.readFile(url).stream().sorted(Comparator.comparing(Book::getAuthor)).collect(Collectors.toList());
+
+        for (Book b : books1) {
+            System.out.println(b.toString());
+        }
+
+        fileService.deleteFile(url);
+
+
+    }
+}
