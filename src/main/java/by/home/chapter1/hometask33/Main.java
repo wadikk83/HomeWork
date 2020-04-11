731bf35003270ca962c30eea719bf1da74145ccd
wadikk83 <wadikk83@gmail.com>
2020-04-11 17:05:00 +0300

    33 задача из 1 книжки

--- src/main/java/by/home/chapter1/hometask33/Main.java
+++ src/main/java/by/home/chapter1/hometask33/Main.java
@@ -1,4 +1,25 @@
 package by.home.chapter1.hometask33;
 
+import java.io.IOException;
+
 public class Main {
+    public static void main(String[] args) {
+        final String directory = "./";
+
+        FileSystemService fs = new FileSystemService();
+
+        try {
+            fs.showFiles(directory);
+        } catch (IOException e) {
+            System.out.println("Ошибка чтения файлов");
+        }
+
+        System.out.println("======================================================================================");
+
+        try {
+            fs.showSubDirectory(directory);
+        } catch (IOException e) {
+            System.out.println("Ошибка чтения каталогов");
+        }
+    }
 }
